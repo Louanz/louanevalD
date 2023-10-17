578ba9b90aec7047af1d0ff3f7c2b4b07a206e6f
Louan <louan.dahan@gmail.com>
2023-10-17 15:45:04 +0200

    Revert "Permissions 2.0"
    This reverts commit ab99d12c5fe98144ee9cbec9b3cf338654f380b4.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Spatie\Permission\Models\Role;
-use Spatie\Permission\Models\Permission;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,23 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Role::create(['name' => 'technicien']);
-        Role::create(['name' => 'administrateur']);
-
-        Permission::create(['name' =>'gère les ordinateurs']);
-        Permission::create(['name' =>'gère les serveurs']);
-        Permission::create(['name' =>'gère les reseaux']);
-
-        $roleTecniciens = Role::findByName('technicien');
-        $roleTecniciens = givePermissionTo(['gère les ordinateurs','gère les serveurs']);
-
-        $roleAdmin = Role::findByName('administrateur');
-        $roleTecniciens = givePermissionTo('gère les reseaux');
-
-        // Pour donnée une role :
-        $user = User::find(1);
-        $user->assignRole('technicien');
-
-
+        //
     }
 }
